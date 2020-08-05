249759ab5afd59abbcf47efa126396bff3070d71
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-05 10:11:24 +0530

    Create boxOfapples.py

---
+++ boxOfapples.py
@@ -0,0 +1,26 @@
+import itertools
+class Solution(object):
+
+    def totalFruit(self, tree):
+        blocks = [(k, len(list(v)))
+                  for k, v in itertools.groupby(tree)]
+
+        ans = i = 0
+        while i < len(blocks):
+            types, weight = set(), 0
+            for j in range(i, len(blocks)):
+                types.add(blocks[j][0])
+                weight += blocks[j][1]
+                if len(types) >= 3:
+                    i = j-1
+                    break
+
+                ans = max(ans, weight)
+            else:
+                break
+
+        return ans
+val=Solution()
+size=int(input())
+tree=list(map(int,input().split()))
+print(val.totalFruit(tree))
